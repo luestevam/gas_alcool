f6af44bf313df15b2c1de30279ce7444e1d056f3
luestevam <luestevam@hotmail.com>
2021-04-30 17:09:58 -0300

    Adcionei ScrollView

--- lib/Home.dart
+++ lib/Home.dart
@@ -6,6 +6,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController _controllerAlcool = TextEditingController();
+  TextEditingController _controllerGasolina = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,48 +18,48 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.blueAccent,
       ),
       body: Container(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(bottom: 32),
-              child: Image.asset("images/logo.png"),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Text(
-                  "Qual melhor opção de abastecimento para seu carro?",
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold
+          padding: EdgeInsets.all(32),
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(32),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: 32),
+                  child: Image.asset("images/logo.png"),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Text(
+                    "Qual melhor opção de abastecimento para seu carro?",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                   ),
-              ),
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  decoration:
+                      InputDecoration(labelText: "Preço Alcool, ex 1.59"),
+                  style: TextStyle(
+                    fontSize: 22,
+                  ),
+                  controller: _controllerAlcool,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  decoration:
+                      InputDecoration(labelText: "Preço Alcool, ex 2.59"),
+                  style: TextStyle(fontSize: 22),
+                  controller: _controllerGasolina,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: RaisedButton(
+                    child: Text("Calcular"),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
             ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Preço Alcool, ex 1.59"
-              ),
-              style: TextStyle(
-                fontSize: 22,
-              ),
-            ),
-
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Preço Alcool, ex 2.59"
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: RaisedButton(
-                child: Text("Calcular"),
-                onPressed: (){},
-              ),
-            ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
